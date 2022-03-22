091a8038ec2a89fd7c44eabecedf57d12ab0d1e7
sammyshows <76188712+sammyshows@users.noreply.github.com>
2022-03-22 20:42:14 +1100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+import { defineNuxtConfig } from 'nuxt3';
+
+export default defineNuxtConfig({
+  buildModules: ['@vueuse/nuxt', '@nuxtjs/strapi'],
+  modules: ['@nuxtjs/tailwindcss'],
+  strapi: {
+    url: process.env.STRAPI_URL || 'https://strapi.warsono.id',
+    prefix: '/api',
+    version: 'v4',
+    cookie: {},
+  },
+  vite: {
+    optimizeDeps: {
+      include: ['markdown-it'],
+    },
+  },
+});
---
+++ pages/blog/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { Post } from "~/types";
+import type { Strapi4Response } from "@nuxtjs/strapi";
+
+const { find } = useStrapi4();
+
+const posts = useState<Post[]>([]);
+try {
+  const response = await find<Strapi4Response<Post>>("posts", {
+    sort: "publishedAt",
+    populate: ["image", "author"],
+  });
+
+  posts.value = response.data;
+} catch (e) {
+  console.log(e);
+}
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl mb-1 font-bold">Blog</h1>
+    <h2 class="text-xl mb-5 font-normal dark:text-gray-400">Latest Posts</h2>
+
+    <BlogPostItem v-for="post in posts" :key="post.id" :post="post" />
+  </div>
+</template>
+
+<style scoped></style>
